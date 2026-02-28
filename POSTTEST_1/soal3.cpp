3f2fc3ac1599fda46309cfad4ed183ff11781f5e
AnnisaNurRaidah(2509106078) <annisanurraidahh@gmail.com>
2026-03-01 00:59:18 +0800

    posttest 1 done

--- POSTTEST_1/soal3.cpp
+++ POSTTEST_1/soal3.cpp
@@ -20,20 +20,21 @@ int main()
 {
     int arr[7] = {2, 3, 5, 7, 11, 13, 17};
 
-    cout << "Array sebelum dibalik:\n";
+    cout << "-----------ARRAY SEBELUM DIBALIK-----------" << endl;
     int *ptr = arr;
     for (int i = 0; i < 7; i++)
     {
-        cout << "Nilai: " << *ptr << " Alamat: " << ptr << endl;
+        cout << "Nilai: " << *ptr << "  Alamat: " << ptr << endl;
         ptr++;
     }
 
     reverseArray(arr, 7);
-    cout << "\nArray setelah dibalik:\n";
+    cout << endl;
+    cout << "-----------ARRAY SETELAH DIBALIK-----------" << endl;
     ptr = arr;
     for (int i = 0; i < 7; i++)
     {
-        cout << "Nilai: " << *ptr << " Alamat: " << ptr << endl;
+        cout << "Nilai: " << *ptr << "  Alamat: " << ptr << endl;
         ptr++;
     }
     return 0;
